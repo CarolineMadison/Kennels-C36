10aa3a0544cbc6a7ab36bb3776a1212c73bc42f0
Caroline Brownlee <brownleecaroline@gmail.com>
2019-11-18 13:40:00 -0600

    finished practice exercise for book 4, chapter 2:

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,47 +1,18 @@
-import React, { Component } from 'react'
-import './Kennel.css'
-import AnimalCard from './animal/AnimalCard';
-import EmployeeCard from './employee/EmployeeCard';
-import LocationCard from './location/LocationCard';
-import OwnerCard from './owner/OwnerCard';
+import React, { Component } from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
+
+import "./Kennel.css"
 
 class Kennel extends Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <h2>Student Kennels<br />
-                        <small>Loving care when you're not there.</small>
-                    </h2>
-                    <address>
-                        Visit Us at the Nashville North Location
-                    <br />500 Puppy Way
-                </address>
-                </div>
-                <div>
-                    <AnimalCard />
-                    <AnimalCard />
-                    <AnimalCard />
-                </div>
-                <div>
-                    <EmployeeCard />
-                    <EmployeeCard />
-                    <EmployeeCard/>
-                </div>
-                <div>
-                    <LocationCard />
-                    <LocationCard />
-                    <LocationCard />
-                    
-                </div>
-                <div>
-                    <OwnerCard />
-                    <OwnerCard />
-                    <OwnerCard />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <>
+        <NavBar />
+        <ApplicationViews />
+      </>
+    )
+  }
 }
 
 export default Kennel
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom"
+import './NavBar.css'
+
+class NavBar extends Component {
+
+  render(){
+
+    return (
+      <header>
+        <h1 className="site-title">Student Kennels<br />
+          <small>Loving care when you're not there.</small>
+        </h1>
+        <nav>
+          <ul className="container">
+            <li><Link className="nav-link" to="/">Home</Link></li>
+            <li><Link className="nav-link" to="/animals">Animals</Link></li>
+            <li><Link className="nav-link" to="/locations">Locations</Link></li>
+            <li><Link className="nav-link" to="/employees">Employees</Link></li>
+            <li><Link className="nav-link" to="/owners">Owners</Link></li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
+export default NavBar;
+
+
+// Create links in your navigation bar for /locations, /employees, and /owners paths.
+// Have each route render the respective component.
