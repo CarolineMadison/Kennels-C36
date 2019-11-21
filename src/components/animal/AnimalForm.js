6b6beab82337467c96542875f50f2e35ce7d0efb
Caroline Brownlee <brownleecaroline@gmail.com>
2019-11-21 13:04:36 -0600

    installed firebase for photo storage and refactored form component for firebase

--- src/components/animal/AnimalForm.js
+++ src/components/animal/AnimalForm.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import AnimalManager from '../../modules/AnimalManager';
 import './AnimalForm.css'
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
 
 class AnimalForm extends Component {
     state = {
         animalName: "",
         breed: "",
-        photo: "",
+        photo: null,
         loadingStatus: false,
     };
 
@@ -32,44 +34,76 @@ class AnimalForm extends Component {
 
             // Create the ani mal and redirect user to animal list
             AnimalManager.post(animal)
-            .then(() => this.props.history.push("/animals"));
+                .then(() => this.props.history.push("/animals"));
         }
     };
 
-    render(){
+    constructNewAnimal = evt => {
+        evt.preventDefault();
+        if (this.state.animalName === "" || this.state.breed === "") {
+            window.alert("Please input an animal name and breed");
+        } else {
+            this.setState({ loadingStatus: true });
+            // step 1: save Image to Firebase
+            const imagesRef = firebase.storage().ref('images');
+            const childRef = imagesRef.child(`${this.state.name}-${Date.now()}`);
+            childRef.put(this.state.photo)
+                // step 2: get url from firebase
+                .then(response => response.ref.getDownloadURL())
+                // step 3: save everything to json server
+                .then(url => {
+                    const newAnimal = {
+                        animalName: "",
+                        breed: "",
+                        photo: "",
+                        photo: url
+                    }
+                    return AnimalManager.post(newAnimal)
+                })
+                .then(() => this.props.history.push('/animals'));
+        }
+    }
 
-        return(
+    render() {
+
+        return (
             <>
-            <form>
-                <fieldset>
-                    <div className="formgrid">
-                        <input
-                        type="text"
-                        required
-                        onChange={this.handleFieldChange}
-                        id="animalName"
-                        placeholder="Animal name"
-                        />
-                        <label htmlFor="animalName">Name</label>
-                        <input
-                        type="text"
-                        required
-                        onChange={this.handleFieldChange}
-                        id="breed"
-                        placeholder="Breed"
-                        />
-                        <label htmlFor="breed">Breed</label>
-                    </div>
-                    <div className="alignRight">
-                        <button
-                        type="button"
-                        disabled={this.state.loadingStatus}
-                        onClick={this.constructNewAnimal}
-                        >Submit</button>
-                    </div>
-                </fieldset>
-            </form>
-        </>
+                <form>
+                    <fieldset>
+                        <div className="formgrid">
+                            <input
+                                type="text"
+                                required
+                                onChange={this.handleFieldChange}
+                                id="animalName"
+                                placeholder="Animal name"
+                            />
+                            <label htmlFor="animalName">Name</label>
+                            <input
+                                type="text"
+                                required
+                                onChange={this.handleFieldChange}
+                                id="breed"
+                                placeholder="Breed"
+                            />
+                            <label htmlFor="breed">Breed</label>
+                            <input
+                            type="file"
+                            placeholder="Animal Photo"
+                            onChange={(e) => this.setState({ photo: e.target.files[0] })}
+                            />
+                        <label>Student Photo</label>
+                        </div>
+                        <div className="alignRight">
+                            <button
+                                type="button"
+                                disabled={this.state.loadingStatus}
+                                onClick={this.constructNewAnimal}
+                            >Submit</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import Kennel from './components/Kennel'
+import * as firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB0FEOKb_SVac2VUN3nPTyb8d1Oy-Ji1uk",
+  authDomain: "kennel-nss-c36-practice.firebaseapp.com",
+  databaseURL: "https://kennel-nss-c36-practice.firebaseio.com",
+  projectId: "kennel-nss-c36-practice",
+  storageBucket: "kennel-nss-c36-practice.appspot.com",
+  messagingSenderId: "645669721525",
+  appId: "1:645669721525:web:3b710560ef5f6c4408b5fc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <Router>
