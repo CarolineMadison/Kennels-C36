cd5fdcbc1e0d46ab1ca8821739de118ce59c0580
Caroline Brownlee <brownleecaroline@gmail.com>
2019-11-25 16:12:44 -0600

    updated Animal Edit Form

--- src/components/animal/AnimalEditForm.js
+++ src/components/animal/AnimalEditForm.js
@@ -1,52 +1,58 @@
 import React, { Component } from "react"
 import AnimalManager from "../../modules/AnimalManager"
 import "./AnimalForm.css"
+import EmployeeManager from "../../modules/EmployeeManager";
 
 class AnimalEditForm extends Component {
-    //set the initial state
-    state = {
-      name: "",
-      breed: "",
-      photo: null,
-      employeeId: "",
-      loadingStatus: true,
+  //set the initial state
+  state = {
+    name: "",
+    breed: "",
+    photo: null,
+    employeeId: "",
+    loadingStatus: true,
+  };
+
+  handleFieldChange = evt => {
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
+
+  updateExistingAnimal = evt => {
+    evt.preventDefault()
+    this.setState({ loadingStatus: true });
+    const editedAnimal = {
+      id: this.props.match.params.animalId,
+      name: this.state.name,
+      photo: this.state.photo,
+      breed: this.state.breed,
+      employeeId: Number(this.state.employeeId)
     };
 
-    handleFieldChange = evt => {
-      const stateToChange = {}
-      stateToChange[evt.target.id] = evt.target.value
-      this.setState(stateToChange)
-    }
-
-    updateExistingAnimal = evt => {
-      evt.preventDefault()
-      this.setState({ loadingStatus: true });
-      const editedAnimal = {
-        id: this.props.match.params.animalId,
-        name: this.state.name,
-        photo: this.state.photo,
-        breed: this.state.breed
-      };
-
-      AnimalManager.update(editedAnimal)
+    AnimalManager.update(editedAnimal)
       .then(() => this.props.history.push("/animals"))
-    }
-    // the form renders but you want to prepopulate the input fields with the animal editing...gotes to get the single animal
-    componentDidMount() {
-      AnimalManager.get(this.props.match.params.animalId)
+  }
+  // the form renders but you want to prepopulate the input fields with the animal editing...gotes to get the single animal
+  componentDidMount() {
+    AnimalManager.get(this.props.match.params.animalId)
       .then(animal => {
-          this.setState({
-            name: animal.name,
-            breed: animal.breed,
-            photo: animal.photo,
-            loadingStatus: false,
-          });
+        this.setState({
+          name: animal.name,
+          breed: animal.breed,
+          photo: animal.photo,
+          employeeId: animal.employeeId,
+          loadingStatus: false,
+        });
       });
-    }
+      EmployeeManager.getAll()
+      .then(employees => this.setState({employees: employees}))
+  }
 
-    render() {
-      return (
-        <>
+  render() {
+    console.log(this.state)
+    return (
+      <>
         <form>
           <fieldset>
             <div className="formgrid">
@@ -61,15 +67,27 @@ class AnimalEditForm extends Component {
               <label htmlFor="animalName">Animal name</label>
 
             </div>
-              <input
-                type="text"
-                required
-                className="form-control"
-                onChange={this.handleFieldChange}
-                id="breed"
-                value={this.state.breed}
-              />
-              <label htmlFor="breed">Breed</label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              onChange={this.handleFieldChange}
+              id="breed"
+              value={this.state.breed}
+            />
+            <label htmlFor="breed">Breed</label>
+            <select
+              className="form-control"
+              id="employeeId"
+              value={this.state.employeeId}
+              onChange={this.handleFieldChange}
+            >
+              {this.state.employees.map(employee =>
+                <option key={employee.id} value={employee.id}>
+                  {employee.name}
+                </option>
+              )}
+            </select>
             <div className="alignRight">
               <button
                 type="button" disabled={this.state.loadingStatus}
@@ -79,9 +97,9 @@ class AnimalEditForm extends Component {
             </div>
           </fieldset>
         </form>
-        </>
-      );
-    }
+      </>
+    );
+  }
 }
 
 export default AnimalEditForm
